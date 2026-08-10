9285f2669f7255988b0f84a9ffccb11b4c855d7f
junjou <romainm.web@gmail.com>
2026-08-10 14:54:40 +0200

    feat: 🎸 add street lighting use case (v1)

---
+++ src/usecases/street-lighting/streetLight.ts
@@ -0,0 +1,90 @@
+import { StellioTemplate } from 'src/interfaces';
+import {
+    getDateProp,
+    getEnumProp,
+    getGeoPropertyProp,
+    getRelationshipProp,
+    getSimpleTextProp,
+    getIntegerProp,
+} from '../../utils/blueprintHelpers';
+
+const entityType = 'Streetlight';
+
+export const StreetlightTemplate: StellioTemplate = {
+    id: `urn:ngsi-ld:${entityType}:Template`,
+    type: 'Template',
+    name: {
+        ...getSimpleTextProp({ title: 'Nom du point lumineux', friendlyAttributeName: 'Nom' }),
+    },
+    description: {
+        ...getSimpleTextProp({ title: 'Description' }),
+    },
+    location: {
+        ...getGeoPropertyProp('Pointer la position du point lumineux sur la carte', 'Point'),
+    },
+    streetAddress: {
+        ...getSimpleTextProp({ title: 'Adresse' }),
+    },
+    circuit: {
+        ...getSimpleTextProp({ title: 'Circuit électrique' }),
+    },
+    controllingMethod: {
+        ...getEnumProp({
+            title: 'Mode de commande',
+            enum: ['Groupe', 'Individuel'],
+        }),
+    },
+    locationCategory: {
+        ...getEnumProp({
+            title: 'Catégorie de localisation',
+            enum: [
+                'Route',
+                'Piste piétonne',
+                'Jardin',
+                'Parking',
+                'Pont',
+                'Tunnel',
+                'Façade',
+                'Playground',
+                'Île centrale',
+            ],
+        }),
+    },
+    refStreetlightGroup: {
+        ...getRelationshipProp('Groupe de points lumineux', 'urn:ngsi-ld:StreetlightGroup:Template'),
+    },
+    refStreetlightControlCabinet: {
+        ...getRelationshipProp('Armoire de commande', 'urn:ngsi-ld:StreetlightControlCabinet:Template'),
+    },
+    lanternHeight: {
+        ...getIntegerProp({ title: 'Hauteur de la lanterne (m)', minimum: 0 }),
+    },
+    streetPoleNum: {
+        ...getSimpleTextProp({ title: 'Numéro de mât' }),
+    },
+    feederPillarNum: {
+        ...getSimpleTextProp({ title: 'Numéro de feeder pillar' }),
+    },
+    dateServiceStarted: {
+        ...getDateProp({ title: 'Date de mise en service' }),
+    },
+    dateLastLampChange: {
+        ...getDateProp({ title: 'Date du dernier changement de lampe' }),
+    },
+    dateLastSwitchingOn: {
+        ...getDateProp({ title: 'Date de la dernière mise sous tension' }),
+    },
+    dateLastSwitchingOff: {
+        ...getDateProp({ title: 'Date de la dernière coupure' }),
+    },
+    jsonSchema: {
+        type: 'Property',
+        value: {
+            schemaType: entityType,
+            title: 'Point lumineux',
+            required: ['name', 'location'],
+            description: `Un point lumineux représente tout type d'éclairage extérieur sur la carte`,
+            minimum: 1,
+        },
+    },
+};
